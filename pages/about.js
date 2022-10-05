2e154d5ff985835f5141620bd127b0e9a961a2de
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-10-05 20:31:02 +0600

    routes added

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function about() {
+    return (
+        <div>
+            <h1>about</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, quaerat?</p>
+            <p>Recusandae fuga nemo rem beatae saepe quas molestias commodi consequatur?</p>
+        </div>
+    )
+}
+
+export default about
